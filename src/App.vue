02d7bc5ed672fe4fe3d7dc10affae798435969d4
Jeremy Morgan <jeremy@Jeremys-Mac-mini.local>
2020-12-12 15:25:07 -0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="jumbotron">
+			<img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="My Cheesy Vue Blog" byline="The blog that took nearly 30 minutes to create!"/>
+				<Articles :articles="items" />
+        </div>
+		</div>
+	</div>
+</div>  
+</template>
+
+<script>
+import Articles from './components/Articles.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import ArticleJSON from './json/articles.json'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Articles
+  },
+  data() {
+    return{
+      items: ArticleJSON
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Articles.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="row">
+		<div class="col-md-3"></div>
+  <div class="col-md-6">
+  <li v-for="item in articles" :key="item.message">
+    <h3><a href="">{{ item.title }}</a></h3>
+    <p><em>Posted: {{ item.date }}</em></p>
+    <p>{{ item.body }}</p>
+    <hr />
+  </li>
+  </div>
+  <div class="col-md-9"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Articles',
+  props: {
+    articles: String
+  }
+}
+</script>
